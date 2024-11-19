05bb92584656b78f11752c572986d1669b957675
Maria Vasilenko <snysic1987@gmail.com>
2024-11-19 15:16:01 +0100

    create classes AppBmi,BmiController,BmiModel

---
+++ src/main/java/masacorporal/AppBmi.java
@@ -0,0 +1,14 @@
+package masacorporal;
+
+public final class AppBmi {
+    public static void main(String[] args) {
+        System.out.println("                          ");
+        System.out.println("Welcome to BMI calculator!");
+        System.out.println("--------------------------");
+        System.out.println("                          ");
+
+        //Instancias necesarias
+
+        //Lanzamos el método que activa el flujo de la aplicación
+    }
+}
